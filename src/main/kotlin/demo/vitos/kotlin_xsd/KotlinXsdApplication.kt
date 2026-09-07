ba0de9ab66834fee213ba3f5400e96cdc93c7477
Vitaly Belotserkovskii <vbelotserkovskiy@bellintegrator.ru>
2026-09-07 11:00:05 +0300

    Initial commit

---
+++ src/main/kotlin/demo/vitos/kotlin_xsd/KotlinXsdApplication.kt
@@ -0,0 +1,11 @@
+package demo.vitos.kotlin_xsd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinXsdApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinXsdApplication>(*args)
+}
---
+++ src/test/kotlin/demo/vitos/kotlin_xsd/KotlinXsdApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.vitos.kotlin_xsd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinXsdApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
